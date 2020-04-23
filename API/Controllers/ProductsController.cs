58fa4a481bee12c2f95709e7db5aee6f313912b0
sanjeev <sanjeevsanjeevbaghel@gmail.com>
2020-04-24 02:35:35 +0530

    InitialCommit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController: ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts(){
+            return "products";
+        }
+    }
+}
