9a7b4bd8a312d5d51c09f30b6cfd7cb38eba067f
sanjeev <sanjeevsanjeevbaghel@gmail.com>
2020-04-30 00:59:41 +0530

    added model classes and navigation bar changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ECommerce';
+  products: IProduct[] = [];
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient
+      .get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe(
+        (res: IPagination) => {
+          this.products = res.data;
+        },
+        (err) => console.log(err)
+      );
+  }
 }
---
+++ client/src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    photoUrl: string;
+    producType: string;
+    productBrand: string;
+  }
