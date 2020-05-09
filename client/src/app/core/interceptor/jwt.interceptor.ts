0314c5b880432e291eb6f558a27c128b615dd4e6
sanjeev <sanjeevsanjeevbaghel@gmail.com>
2020-05-10 00:48:33 +0530

    Updated to anugular 9

--- client/src/app/core/interceptor/jwt.interceptor.ts
+++ client/src/app/core/interceptor/jwt.interceptor.ts
@@ -1,6 +1,8 @@
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class JwtInterceptor implements HttpInterceptor {
     intercept(
         req: HttpRequest<any>,
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Build client project with "ng build --prod". This will place all the dependencies to wwwroot folder of API project
+Run Redis server
+Run API project
+Browse https://localhost:5001
